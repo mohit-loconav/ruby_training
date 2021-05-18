ba2502d179c3215fce5a71a6a304969c791daf0b
Mohit Singh <mohit.s@loconav.com>
2021-05-18 15:06:56 +0530

    remove unnecessary attr_reader, implemented auto_increment

--- exercises/5_account_balance.rb
+++ exercises/5_account_balance.rb
@@ -1,11 +1,12 @@
 class Account
-  attr_reader :name, :account_no, :balance
   attr_accessor :name, :balance
+  @@total = 1
 
-  def initialize(account_no, name, balance)
-    @account_no = account_no
+  def initialize(name, balance)
+    @account_no = @@total
     @name = name
     @balance = balance
+    @@total += 1
   end
 
   def deposit(amount)
@@ -33,8 +34,8 @@ query = gets.chomp
 
 acc1, acc2, transfer = query.split(" ")
 
-acc1 = Account.new(1, acc1.split(":")[0].gsub("\"", ""), acc1.split(":")[1].to_i)
-acc2 = Account.new(2, acc2.split(":")[0].gsub("\"", ""), acc2.split(":")[1].to_i)
+acc1 = Account.new(acc1.split(":")[0].gsub("\"", ""), acc1.split(":")[1].to_i)
+acc2 = Account.new(acc2.split(":")[0].gsub("\"", ""), acc2.split(":")[1].to_i)
 
 transfer_amount = transfer.split(":")[1].to_i
 
