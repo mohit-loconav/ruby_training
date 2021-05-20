79362b9ab708b076f8fbc2c7ac9c043f16494c58
Mohit Singh <mohit.s@loconav.com>
2021-05-20 13:18:31 +0530

    hardcoded time initialization removed

--- exercises/17_sum_time.rb
+++ exercises/17_sum_time.rb
@@ -1,30 +1,25 @@
 require "time"
+
 def sum_time(time_str_1, time_str_2)
-  time_arr_1 = time_str_1.split(":").map {|el| el.to_i}
   time_arr_2 = time_str_2.split(":").map {|el| el.to_i}
 
-  time_1 = Time.new(2021, 5, 1, time_arr_1[0], time_arr_1[1], time_arr_1[2])
+  time_1 = Time.parse(time_str_1);
   #convert time two to seconds
   seconds = time_arr_2[0]*60*60 + time_arr_2[1]*60 + time_arr_2[2]
+  sum = time_1 + seconds
+  days = sum.day-time_1.day
 
-  time_1 + seconds
+  days > 0 ? "#{days} day & #{sum.strftime("%T")}" : sum.strftime("%T");
 end
 
 #to validate 24 hour time string
 def validate_time(time)
-  time=~/^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]/
+  puts time=~/^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]/
 end
-
-#generate a time string as per the question
-def format_time(time)
-  day = time.strftime("%e").to_i-1
-  day>0 ? "#{day} day & #{time.strftime("%T")}" : time.strftime("%T");
-end
-
 
 time_str_1, time_str_2 = ARGV[0], ARGV[1]
 if(validate_time(time_str_1) && validate_time(time_str_2))
-  puts format_time(sum_time(time_str_1, time_str_2))
+  sum_time(time_str_1, time_str_2)
 else
   puts "Invalid 24-hour time value"
 end
--- exercises/19_sum_time_many.rb
+++ exercises/19_sum_time_many.rb
@@ -1,14 +1,18 @@
+require "time"
+
 def sum_time(time_list)
   time_arr_1 = time_list[0].split(":").map {|el| el.to_i}
-  time_sum = Time.new(2021, 5, 1, time_arr_1[0], time_arr_1[1], time_arr_1[2])
+  time_1 = Time.parse(time_list[0])
 
+  seconds = 0
   for time in time_list[1, time_list.length-1]
     time_arr = time.split(":").map {|el| el.to_i}
-    seconds = time_arr[0]*60*60 + time_arr[1]*60 + time_arr[2]  
-    time_sum += seconds
+    seconds += time_arr[0]*60*60 + time_arr[1]*60 + time_arr[2]  
   end
-
-  time_sum
+  time_sum = time_1 + seconds
+  days = time_sum.day-time_1.day
+  
+  days > 0 ? "#{days} day & #{time_sum.strftime("%T")}" : time_sum.strftime("%T");
 end
 
 #to validate 24 hour time string
@@ -16,23 +20,16 @@ def validate_time(time)
   time=~/^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]/
 end
 
-#generate a time string as per the question
-def format_time(time)
-  day = time.strftime("%e").to_i-1
-  day>0 ? "#{day} day & #{time.strftime("%T")}" : time.strftime("%T");
-end
-
-
 validate = true
 for time in ARGV
-  if validate_time(time) == false
+  unless validate_time(time)
     validate = false
     break
   end
 end
 
 if(validate)
-  puts format_time(sum_time(ARGV))
+  puts sum_time(ARGV)
 else
   puts "Invalid 24-hour time value"
 end
